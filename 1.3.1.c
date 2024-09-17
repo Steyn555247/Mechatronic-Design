280dceaf7be96687a42b6008931f2fea2a4f3424
Steyn555247 <steyn.knollema@gmail.com>
2024-09-17 14:48:13 -0400

    1.3.1.c

--- 1.3.1.c
+++ 1.3.1.c
@@ -5,7 +5,7 @@
  */
 
 #include "MEAM_general.h"  // includes the resources included in the MEAM_general.h file
-#define MaxTime 6250
+#define Maxtime 6250
 
 
 int main(void)
@@ -15,7 +15,7 @@ int main(void)
 	TCCR3B = 0x03; //Scales down timer to 250KHz (0000 0011)
 
 	for(;;){ //start infinite for loop
-		if (TCNT3>MaxTime) // if the time of timer is higher than MaxTime
+		if (TCNT3>Maxtime) // if the time of timer is higher than MaxTime
 		{
 			toggle(PORTB, 1); //toggle PORTB
 			TCNT3 = 0; //rest TCNT3
